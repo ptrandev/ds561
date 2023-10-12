c67d30031500e120cf0b07cffbdcb3fe1030c3f4
Phillip Tran <ptrandev@protonmail.com>
2023-10-11 23:28:53 -0400

    hw04 modifications

--- HW04/web-server/main.py
+++ HW04/web-server/main.py
@@ -1,13 +1,23 @@
 from google.cloud import storage
 from google.cloud import pubsub_v1
-import functions_framework
+from google.cloud import logging
+from flask import Flask, request
 
-@functions_framework.http
-def get_file(request):
-  # set up pub sub
-  publisher = pubsub_v1.PublisherClient()
-  topic_path = publisher.topic_path('ds561-trial-project', 'banned-countries-topic')
+app = Flask(__name__)
 
+HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
+
+# set up pub sub
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path('ds561-trial-project', 'banned-countries-topic')
+
+# set up logging; log into web-server-hw04
+client = logging.Client()
+logging_client = client.logger('web-server-hw04')
+
+@app.route('/', defaults={'path': ''}, methods=HTTP_METHODS)
+@app.route('/<path:path>', methods=HTTP_METHODS)
+def get_file(path):
   # get country from header X-country
   country = request.headers.get('X-country')
 
@@ -18,33 +28,35 @@ def get_file(request):
   # if the country is banned, publish to banned-countries topic
   if country and country.lower() in banned_countries:
     publisher.publish(topic_path, country.encode('utf-8'))
-    print('Banned country:', country)
+    logging_client.log_text(f'Banned country: {country}')
     return 'Banned country', 400
 
   # only accept GET method
   if request.method != 'GET':
-    print('Method not implemented:', request.method)
+    logging_client.log_text(f'Method not implemented: {request.method}')
     return 'Method not implemented', 501
 
   # get dirname/filename.html from path
-  # path should be function_name/bucket_name/dirname/filename.html
-  bucket_name = request.path.split('/')[1]
-  file_name = '/'.join(request.path.split('/')[2:])
+  # path should be bucket_name/dirname/filename.html
+  bucket_name = path.split('/')[0]
+  file_name = '/'.join(path.split('/')[1:])
 
   if file_name is None:
     print('file_name is required')
     return 'file_name is required', 400
   
+  if bucket_name is None:
+    print('bucket_name is required')
+    return 'bucket_name is required', 400
+  
   # get file from bucket
   storage_client = storage.Client()
-
   bucket = storage_client.bucket(bucket_name)
-
   blob = bucket.blob(file_name)
 
   if blob.exists():
     blob_content = blob.download_as_string()
     return blob_content, 200, {'Content-Type': 'text/html; charset=utf-8'}
   
-  print('File not found:', f'{bucket_name}/{file_name}')
+  logging_client.log_text(f'File not found: {bucket_name}/{file_name}')
   return 'File not found', 404
---
+++ HW04/web-server/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/ptrandev
+source env/bin/activate
+flask --app main run --host=0.0.0.0
