3a39f6aabe2e033510cfc606f743c7b149e3ee06
Phillip Tran <ptrandev@protonmail.com>
2023-10-16 17:54:25 -0400

    Update startup.sh

--- HW04/web-server/startup.sh
+++ HW04/web-server/startup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-cd /home/ptrandev
-source env/bin/activate
-flask --app main run --host=0.0.0.0
+# download all files from ds561-ptrandev-hw04 bucket only if they don't exist yet
+gsutil -m cp -r gs://ds561-ptrandev-hw04/ /home/ptrandev/
+
+# go to the directory where the flask app is located
+cd /home/ptrandev/ds561-ptrandev-hw04
+
+# install dependencies from requirements.txt
+apt install python3-pip -y
+pip3 install -r requirements.txt
+
+# run the flask app
+python3 -m flask --app main run --host=0.0.0.0
