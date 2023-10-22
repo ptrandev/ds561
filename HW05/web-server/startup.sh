7061347aab640c1602c7540042512e95e50111f8
Phillip Tran <ptrandev@protonmail.com>
2023-10-21 23:15:59 -0400

    Update startup.sh

--- HW05/web-server/startup.sh
+++ HW05/web-server/startup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # check if the directory exists; if not, copy the files from the bucket to the directory
-if [ -d "/root/ds561-ptrandev-hw04" ]; then
-    echo "Directory /root/ds561-ptrandev-hw04 exists."
+if [ -d "/root/ds561-ptrandev-hw05" ]; then
+    echo "Directory /root/ds561-ptrandev-hw05 exists."
 else
     # copy the files from the bucket to the directory
-    gsutil -m cp -r gs://ds561-ptrandev-hw04/ /root/
+    gsutil -m cp -r gs://ds561-ptrandev-hw05/ /root/
 fi
 
 # go to the directory where the flask app is located
-cd /root/ds561-ptrandev-hw04
+cd /root/ds561-ptrandev-hw05
 
 # install dependencies from requirements.txt
 apt install python3-pip -y
