af6b46d002ad42ccad1eef370b3b8ab1a044472f
Phillip Tran <ptrandev@protonmail.com>
2023-11-08 16:34:31 -0500

    Update top_links.py

--- HW07/top_links.py
+++ HW07/top_links.py
@@ -1,38 +1,46 @@
 import apache_beam as beam
 import re
 import argparse
+import time
 
-class LinkCount(beam.DoFn):
+
+class IncomingLinkCount(beam.DoFn):
     def process(self, element):
         regex = re.compile(r'<a HREF="(\d+).html">')
         links = re.findall(regex, element)
         for link in links:
-            yield (link, 1)
+            yield (int(link), 1)
 
-def run(bucket, directory):
+def run(input_files):
+    start = time.time()
+
     with beam.Pipeline() as p:
-        # Read HTML files from your Cloud Storage bucket
-        input_files = f"gs://{bucket}/{directory}*.html"
-
         incoming_links = (
             p
             | "Read HTML Files" >> beam.io.ReadFromText(input_files)
-            | "Count Incoming Links" >> beam.ParDo(LinkCount())
-            | "Group by URL" >> beam.GroupByKey()
-            | "Sum Incoming Links" >> beam.CombineValues(sum)
-        )
-
-        outgoing_links = (
-            incoming_links
-            | "Flatten Incoming Links" >> beam.FlatMap(lambda x: [(url, 0) for url in x[0]])
-            | "Count Outgoing Links" >> beam.CombinePerKey(sum)
+            | "Count Incoming Links" >> beam.ParDo(IncomingLinkCount())
+            | "Sum and Combine Incoming Links" >> beam.CombinePerKey(sum)
         )
 
         # Find and print the top 5 files with the most incoming links
-        incoming_links | "Top 5 Incoming Links" >> beam.transforms.combiners.Top.Largest(5, key=lambda x: x[1]) | "Print Top 5 Incoming Links" >> beam.Map(print)
+        incoming_links | "Top 5 Incoming Links" >> beam.transforms.combiners.Top.Largest(
+            5, key=lambda x: x[1]
+        ) | "Print Top 5 Incoming Links" >> beam.Map(
+            print
+        )
 
-        # Find and print the top 5 files with the most outgoing links
-        outgoing_links | "Top 5 Outgoing Links" >> beam.transforms.combiners.Top.Largest(5, key=lambda x: x[1]) | "Print Top 5 Outgoing Links" >> beam.Map(print)
+    print(f"Total time: {time.time() - start} seconds")
+
+
+def run_cloud(bucket, directory):
+    print("Running on the cloud...")
+    run(f"gs://{bucket}/{directory}*.html")
+
+
+def run_local(directory):
+    print("Running locally...")
+    run(f"{directory}0.html")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -51,4 +59,18 @@ if __name__ == "__main__":
         help="The directory path to the HTML files in the bucket (default: html/))",
     )
 
-    run(**vars(parser.parse_args()))
+    parser.add_argument(
+        "--local",
+        type=bool,
+        default=False,
+        help="Run the pipeline locally (default: False)",
+    )
+
+    bucket = parser.parse_args().bucket
+    directory = parser.parse_args().directory
+    local = parser.parse_args().local
+
+    if local:
+        run_local(directory)
+    else:
+        run_cloud(bucket, directory)
