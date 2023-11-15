845497b028c5dd11bc7b02af969041f888becb7a
Phillip Tran <ptrandev@protonmail.com>
2023-11-15 00:45:26 -0500

    added zone to startup script

--- HW08/web-server/startup.sh
+++ HW08/web-server/startup.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # check if the directory exists; if not, copy the files from the bucket to the directory
-if [ -d "/root/ds561-ptrandev-hw04" ]; then
-    echo "Directory /root/ds561-ptrandev-hw04 exists."
+if [ -d "/root/ds561-ptrandev-hw08" ]; then
+    echo "Directory /root/ds561-ptrandev-hw08 exists."
 else
     # copy the files from the bucket to the directory
-    gsutil -m cp -r gs://ds561-ptrandev-hw04/ /root/
+    gsutil -m cp -r gs://ds561-ptrandev-hw08/ /root/
 fi
 
 # go to the directory where the flask app is located
-cd /root/ds561-ptrandev-hw04
+cd /root/ds561-ptrandev-hw08
 
 # install dependencies from requirements.txt
 apt install python3-pip -y
 pip3 install -r requirements.txt
+
+# add environment variable for GCP zone
+export GCP_ZONE=us-east4-a
 
 # run the flask app
 python3 main.py
