afa0885f4db6c929564b30da43bc75097ef15fa2
Phillip Tran <ptrandev@protonmail.com>
2023-11-15 00:45:35 -0500

    now adding zone as header

--- HW08/web-server/main.py
+++ HW08/web-server/main.py
@@ -3,6 +3,7 @@ from google.cloud import pubsub_v1
 from google.cloud import logging
 from flask import Flask, request
 from waitress import serve
+import os
 
 app = Flask(__name__)
 
@@ -15,6 +16,9 @@ topic_path = publisher.topic_path('ds561-trial-project', 'banned-countries-topic
 # set up logging; log into web-server-hw04
 client = logging.Client()
 logging_client = client.logger('web-server-hw04')
+
+# set up environment variables
+GCP_ZONE = os.environ.get('GCP_ZONE')
 
 @app.route('/', defaults={'path': ''}, methods=HTTP_METHODS)
 @app.route('/<path:path>', methods=HTTP_METHODS)
@@ -30,12 +34,12 @@ def get_file(path):
   if country and country.lower() in banned_countries:
     publisher.publish(topic_path, country.encode('utf-8'))
     logging_client.log_text(f'Banned country: {country}')
-    return 'Banned country', 400
+    return 'Banned country', 400, {'X-GCP-ZONE': GCP_ZONE}
 
   # only accept GET method
   if request.method != 'GET':
     logging_client.log_text(f'Method not implemented: {request.method}')
-    return 'Method not implemented', 501
+    return 'Method not implemented', 501, {'X-GCP-ZONE': GCP_ZONE}
 
   # get dirname/filename.html from path
   # path should be bucket_name/dirname/filename.html
@@ -44,11 +48,11 @@ def get_file(path):
 
   if file_name is None:
     print('file_name is required')
-    return 'file_name is required', 400
+    return 'file_name is required', 400, {'X-GCP-ZONE': GCP_ZONE}
   
   if bucket_name is None:
     print('bucket_name is required')
-    return 'bucket_name is required', 400
+    return 'bucket_name is required', 400, {'X-GCP-ZONE': GCP_ZONE}
   
   # get file from bucket
   storage_client = storage.Client()
@@ -57,9 +61,9 @@ def get_file(path):
 
   if blob.exists():
     blob_content = blob.download_as_string()
-    return blob_content, 200, {'Content-Type': 'text/html; charset=utf-8'}
-  
+    return blob_content, 200, {'Content-Type': 'text/html; charset=utf-8', 'X-GCP-ZONE': GCP_ZONE}
+
   logging_client.log_text(f'File not found: {bucket_name}/{file_name}')
-  return 'File not found', 404
+  return 'File not found', 404, {'X-GCP-ZONE': GCP_ZONE}
 
 serve(app, host='0.0.0.0', port=5000)
