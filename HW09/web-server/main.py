476e3f2721e1652c5fdb349801a52889e93ec471
Phillip Tran <ptrandev@protonmail.com>
2023-11-23 23:43:54 -0500

    done

--- HW09/web-server/main.py
+++ HW09/web-server/main.py
@@ -4,6 +4,8 @@ from google.cloud import logging
 from flask import Flask, request
 from waitress import serve
 
+import logging
+
 app = Flask(__name__)
 
 HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
@@ -12,9 +14,8 @@ HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'T
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path('ds561-trial-project', 'banned-countries-topic')
 
-# set up logging; log into web-server-hw09
-client = logging.Client()
-logging_client = client.logger('web-server-hw09')
+# set up logging
+logging.getLogger().setLevel(logging.INFO)
 
 @app.route('/', defaults={'path': ''}, methods=HTTP_METHODS)
 @app.route('/<path:path>', methods=HTTP_METHODS)
@@ -29,14 +30,12 @@ def get_file(path):
   # if the country is banned, publish to banned-countries topic
   if country and country.lower() in banned_countries:
     publisher.publish(topic_path, country.encode('utf-8'))
-    logging_client.log_text(f'Banned country: {country}')
-    print(f'Banned country: {country}')
+    logging.info(f'Banned country: {country}')
     return 'Banned country', 400
 
   # only accept GET method
   if request.method != 'GET':
-    logging_client.log_text(f'Method not implemented: {request.method}')
-    print(f'Method not implemented: {request.method}')
+    logging.info(f'Method not implemented: {request.method}')
     return 'Method not implemented', 501
 
   # get dirname/filename.html from path
@@ -45,11 +44,11 @@ def get_file(path):
   file_name = '/'.join(path.split('/')[1:])
 
   if file_name is None:
-    print('file_name is required')
+    logging.info('file_name is required')
     return 'file_name is required', 400
   
   if bucket_name is None:
-    print('bucket_name is required')
+    logging.info('bucket_name is required')
     return 'bucket_name is required', 400
   
   # get file from bucket
@@ -61,8 +60,7 @@ def get_file(path):
     blob_content = blob.download_as_string()
     return blob_content, 200, {'Content-Type': 'text/html; charset=utf-8'}
   
-  logging_client.log_text(f'File not found: {bucket_name}/{file_name}')
-  print(f'File not found: {bucket_name}/{file_name}')
+  logging.info(f'File not found: {bucket_name}/{file_name}')
   return 'File not found', 404
 
 serve(app, host='0.0.0.0', port=5000)
