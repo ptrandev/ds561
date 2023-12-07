07bd11b4515860114da81d0f0c175805dee52c6a
Phillip Tran <ptrandev@protonmail.com>
2023-12-06 22:05:47 -0500

    fixing errors

--- HW10/web-server/main.py
+++ HW10/web-server/main.py
@@ -67,7 +67,7 @@ with pool.connect() as conn:
                 is_banned BOOLEAN,
                 country VARCHAR(255),
                 client_ip VARCHAR(15),
-                gender ENUM('Male', 'Female')
+                gender ENUM('Male', 'Female'),
                 age ENUM('0-16', '17-25', '26-35', '36-45', '46-55', '56-65', '66-75', '76+'),
                 income ENUM('0-10k', '10k-20k', '20k-40k', '40k-60k', '60k-100k', '100k-150k', '150k-250k', '250k+')
             );
